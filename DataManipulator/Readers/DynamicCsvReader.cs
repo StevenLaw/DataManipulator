fbd1f90299e36731148f9248275d321dd50677ad
Steven Law <stevlawonyou@yahoo.com>
2020-08-25 13:14:57 -0600

    Add DunamicCsvReader and consolidate packages

---
+++ DataManipulator/Readers/DynamicCsvReader.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper;
+using DataManipulator.Interfaces;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace DataManipulator.Readers
+{
+    public class DynamicCsvReader : IReader<object>
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the csv file has a header.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has header; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasHeader { get; set; }
+        /// <summary>
+        /// Gets or sets the path.
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicCsvReader"/> class.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="hasHeader">if set to <c>true</c> the csv file has a header.</param>
+        public DynamicCsvReader(string path, bool hasHeader)
+        {
+            Path = path;
+            HasHeader = hasHeader;
+        }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <returns>
+        /// the data.
+        /// </returns>
+        public IEnumerable<object> GetData()
+        {
+            using (var reader = new StreamReader(Path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.HasHeaderRecord = HasHeader;
+                return csv.GetRecords<dynamic>().ToArray();
+            }
+        }
+    }
+}
---
+++ DataManipulatorTests/Readers/DynamicCsvReaderTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DataManipulator.Readers.Tests
+{
+    [TestClass()]
+    public class DynamicCsvReaderTests
+    {
+        [TestMethod()]
+        public void DynamicCsvReaderTest()
+        {
+            var reader = new DynamicCsvReader("MOCK_DATA.csv", true);
+
+            Assert.IsTrue(reader.GetData().Count() > 0);
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            // arrange
+            var reader = new DynamicCsvReader("MOCK_DATA.csv", true);
+
+            // act
+            var data = reader.GetData();
+
+            // assert
+            Assert.AreEqual(1000, data.Count());
+        }
+    }
+}
